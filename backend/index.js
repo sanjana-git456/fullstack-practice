ce0609c5217804eb1c7f67caf481fc0eb771af4e
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-01 20:37:26 +0530

    setup

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next()
+})
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log('MongoDB connection error: ', err))
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
