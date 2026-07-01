734b3c64fb0ad099fae2a850ba9123fdad14ca2d
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-01 23:20:15 +0530

    app.listen working

--- backend/index.js
+++ backend/index.js
@@ -13,3 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 })
+
+app.get('/',(req,res) => {
+    res.send('Hello from Express')
+})
